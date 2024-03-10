bbf7416fb4cd540c510862e6a382cd1df105af35
dachaDacha94 <160251629+dachaDacha94@users.noreply.github.com>
2024-03-10 19:49:57 +0300

    final project

---
+++ mrFlip/index.js
@@ -0,0 +1,11 @@
+function clickMe() {
+  let btnClick = document.getElementById("letterBtn");
+  console.log(letterBtn.letterBtn);
+}
+
+function changeMe() {
+  let catImage = document.getElementById("changeImg");
+  catImage.src =
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fscientificrussia.ru%2Farticles%2Fkotiki-i-my&psig=AOvVaw0EWL9yywm8xO10wxdTLf-j&ust=1707672174363000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIiNsNykoYQDFQAAAAAdAAAAABAE";
+}
+ 
